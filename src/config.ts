710b06afc68da5900d8486de4f0e0cf682b3f2ce
Quentin BONHOURE <bonhoure.quentin@gmail.com>
2026-04-24 15:11:47 +0200

    fix: delet process.env

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const config = {
-  port: Number(process.env.PORT ?? 3000),
-  stocksApiUrl: process.env.STOCKS_API_URL ?? "https://keligmartin.github.io/api/stocks.json",
-  requestTimeoutMs: Number(process.env.REQUEST_TIMEOUT_MS ?? 10000)
+  port: 3000,
+  stocksApiUrl: "https://keligmartin.github.io/api/stocks.json",
+  requestTimeoutMs: 10000
 };
